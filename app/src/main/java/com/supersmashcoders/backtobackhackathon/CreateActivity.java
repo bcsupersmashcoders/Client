598704c586a524f7b4d23609ef8abb5fb946cdca
Isaac Montenegro <isaacmj@yahoo.com>
2015-06-13 09:59:38 -0600

    Added create screen

---
+++ app/src/main/java/com/supersmashcoders/backtobackhackathon/CreateActivity.java
@@ -0,0 +1,92 @@
+package com.supersmashcoders.backtobackhackathon;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+
+public class CreateActivity extends ActionBarActivity {
+
+    Calendar myCalendar = Calendar.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+
+        final EditText startDate = (EditText) findViewById(R.id.input_start_date);
+        startDate.setOnClickListener(getDatePickerListener(startDate));
+
+        EditText endDate = (EditText) findViewById(R.id.input_end_date);
+        endDate.setOnClickListener(getDatePickerListener(endDate));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_create, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private View.OnClickListener getDatePickerListener(final EditText exitText) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(CreateActivity.this,
+                        new DatePickerListener(myCalendar, exitText),
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        };
+    }
+
+    private static class DatePickerListener implements DatePickerDialog.OnDateSetListener {
+        private Calendar calendar;
+        private EditText editTextDisplay;
+
+        public DatePickerListener(Calendar calendar, EditText editTextDisplay) {
+            this.calendar = calendar;
+            this.editTextDisplay = editTextDisplay;
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, monthOfYear);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+        private void updateLabel() {
+            String myFormat = "MM/dd/yy"; //In which you need put here
+            SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+            editTextDisplay.setText(sdf.format(calendar.getTime()));
+        }
+    };
+}
--- app/src/main/java/com/supersmashcoders/backtobackhackathon/MainActivity.java
+++ app/src/main/java/com/supersmashcoders/backtobackhackathon/MainActivity.java
@@ -1,22 +1,18 @@
 package com.supersmashcoders.backtobackhackathon;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity
@@ -99,8 +95,9 @@ public class MainActivity extends ActionBarActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_create) {
+            Intent createIntent = new Intent(this, CreateActivity.class);
+            startActivity(createIntent);
         }
 
         return super.onOptionsItemSelected(item);
